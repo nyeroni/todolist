e3af9d2ea5fd50121fd15cf7994784185ef118ad
nyeroni <nir2y@naver.com>
2024-02-11 03:12:58 +0900

    Feat: 빈 칸 입력 시 등록 안되게 설정

--- src/main/java/yerong/todolist/controller/TodoController.java
+++ src/main/java/yerong/todolist/controller/TodoController.java
@@ -1,15 +1,26 @@
 package yerong.todolist.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import yerong.todolist.domain.ToDo;
 import yerong.todolist.domain.dto.ToDoRequestDto;
 import yerong.todolist.domain.dto.ToDoResponseDto;
+import yerong.todolist.repository.ToDoRepository;
 import yerong.todolist.service.TodoService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,28 +28,43 @@ import java.util.List;
 public class TodoController {
 
     private final TodoService todoService;
+    private final MessageSource messageSource;
+
     @GetMapping("/")
+    public String home(Model model){
+        return "redirect:/todo";
+    }
+    @GetMapping("/todo")
     public String index(Model model){
         List<ToDoResponseDto> toDoList = todoService.getToDoList();
         model.addAttribute("todoList", toDoList);
-        model.addAttribute("todo", new ToDoResponseDto());
+        model.addAttribute("todo", new ToDoRequestDto());
         return "index";
     }
     @PostMapping("/todo/addTodo")
-    public String insertTodo(ToDoRequestDto dto){
+    public String insertTodo(@Valid @ModelAttribute ToDoRequestDto dto, BindingResult bindingResult){
+        if (StringUtils.isEmpty(dto.getMessage())) {
+            String errorMessage = messageSource.getMessage("error.message.required", null, LocaleContextHolder.getLocale());
+            bindingResult.addError(new FieldError("dto", "message", errorMessage));
+        }
+        if (bindingResult.hasErrors()) {
+
+            return "index";
+        }
+
         todoService.insertToDoList(dto);
-        return "redirect:/";
+        return "redirect:/todo";
     }
 
     @PostMapping("/todo/{id}/update")
     public String updateStatus(@PathVariable("id") Long id){
         todoService.updateStatusToDoList(id);
-        return "redirect:/";
+        return "redirect:/todo";
     }
 
     @PostMapping("/todo/{id}/delete")
     public String deleteTodo(@PathVariable("id") Long id){
         todoService.deleteToDoList(id);
-        return "redirect:/";
+        return "redirect:/todo";
     }
 }
